8492366b667c489729206f4236e8039e9bf9c49a
rn404 <nekonekonekona@gmail.com>
2022-01-15 22:24:01 +0900

    fix todo functions

--- src/todo.ts
+++ src/todo.ts
@@ -7,18 +7,7 @@ import { addItem, getCurrentFile, parse as parseLog, update } from './log.ts';
 import { MemoItem } from './models/MemoItem.ts';
 import { LOG_DIR } from './const.ts'
 
-const { _: args } = parse(Deno.args);
-const [topCommands, subCommands] = args;
-
-console.log('DEBUG: ', parse(Deno.args));
-console.log('DEBUG: ', ...args);
-
-// NOTE そのうちいらなくなるかも
-if (topCommands !== 'todo') {
-  Deno.exit(0);
-}
-
-if (subCommands === 'list') {
+const listCommand = async (): Promise<void> => {
   const { createdAt, data } = await getCurrentFile(LOG_DIR);
   const { tasks, memos } = parseLog(data);
 
@@ -28,6 +17,7 @@ if (subCommands === 'list') {
   const spacer = ' ';
 
   if (isNoData) {
+    prompt('There is no data...')
     Deno.exit(0);
   }
 
@@ -64,11 +54,29 @@ if (subCommands === 'list') {
   Deno.exit(0);
 }
 
-if (subCommands !== undefined) {
-  const [, content] = args;
-  const newItem: MemoItem = new MemoItem(content.toString());
-
+const addMemoCommand = async (content: string): Promise<void> => {
+  const newItem: MemoItem = new MemoItem(content);
   const { path, fileName, data } = await getCurrentFile(LOG_DIR);
   const newLog = await addItem(newItem, data);
   await update(newLog, path, fileName);
+}
+
+const { _: args } = parse(Deno.args);
+const [topCommands, subCommands] = args;
+
+console.log('DEBUG: ', parse(Deno.args));
+console.log('DEBUG: ', ...args);
+
+// NOTE そのうちいらなくなるかも
+if (topCommands !== 'todo') {
+  Deno.exit(0);
+}
+
+if (subCommands === 'list') {
+  listCommand()
+}
+
+if (subCommands !== undefined) {
+  const [, content] = args;
+  addMemoCommand(content.toString())
 }
