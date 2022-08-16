916698dfffaa26caf2b4220b6a11974c5d0bdc2f
rn404 <nekonekonekona@gmail.com>
2022-08-16 17:52:59 +0900

    Create LogFileName Factory

--- src/models/LogFileName.ts
+++ src/models/LogFileName.ts
@@ -1,13 +1,12 @@
-import { format } from '../dependencies.ts';
 import { LOG_FILE_EXT } from '../const.ts';
 import { DateString } from './Date.ts';
 
 export class LogFileName {
   public name: DateString;
   public withExtension: `${DateString}.${typeof LOG_FILE_EXT}`;
-  constructor(targetDay: Date) {
-    const fileName = format(targetDay, 'yyyy-MM-dd', {}) as DateString;
-    this.name = fileName;
-    this.withExtension = `${fileName}.${LOG_FILE_EXT}`;
+
+  constructor(date: DateString) {
+    this.name = date;
+    this.withExtension = `${date}.${LOG_FILE_EXT}`;
   }
 }
---
+++ src/models/factory/LogFileName.ts
@@ -0,0 +1,31 @@
+import { format } from '../../dependencies.ts';
+import { DateString } from '../Date.ts';
+import { LogFileName } from '../LogFileName.ts';
+
+export class LogFileNameFactory {
+  #date: DateConstructor;
+
+  constructor(dateConstructor: DateConstructor) {
+    this.#date = dateConstructor;
+  }
+
+  private formatToDateString(date: Date): DateString {
+    return format(date, 'yyyy-MM-dd', {}) as DateString;
+  }
+
+  public create(date?: Date): LogFileName;
+  public create(dateString?: DateString): LogFileName;
+  public create(target?: DateString | Date): LogFileName {
+    if (target === undefined) {
+      const date = new this.#date();
+      return new LogFileName(this.formatToDateString(date));
+    }
+
+    if (target instanceof Date) {
+      return new LogFileName(this.formatToDateString(target));
+    }
+
+    const date = new this.#date(target);
+    return new LogFileName(this.formatToDateString(date));
+  }
+}
